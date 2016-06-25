039ecc8115fdd93f0f1c3e57cd30b5c6d354b494
Tait <Taitwayland@gmail.com>
2016-06-25 13:33:27 -0500

    node compression and gitignore node modules

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require('express');
+var compress = require('compression');
 var app = express();
 
 app.use(express.static(__dirname + '/'));
-app.use(express.compress());  
+app.use(compress());
 
 app.listen(process.env.PORT || 3000);
