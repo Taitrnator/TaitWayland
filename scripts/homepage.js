23d5d66d0cedf617fb3f45e462d71d20ee1d8465
Tait <Taitwayland@gmail.com>
2016-06-26 17:49:19 -0500

    async action

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-
   // cool shit with text
   function coolText(e, fps) {
        $(e).addClass("fadeIn");
@@ -8,10 +7,23 @@ $(document).ready(function() {
        }
   }
 
-  coolText(".header-1", 25);
+  setTimeout(function() { coolText(".header-1", 44); });
   setTimeout(function() { coolText(".header-2", 44); }, 700);
   setTimeout(function() { coolText(".header-3"); }, 2500);
 
+  // stupid unnecessary shit
+   function coolMaskers(h1, h2) {
+     $(window).on('scroll', function() {
+       var height = $(document).scrollTop();
+       var masker1height = Math.abs($(h1).offset().top - height);
+       if (masker1height < 530) {
+         coolText(h1, 25);
+         setTimeout(function() { coolText(h2, 25); }, 1000);
+         $(window).off('scroll');
+      }
+   });
+   }
+  coolMaskers(".masker1", ".masker2");
 
   // menu functionality
   $(".hamburger").on('click', function() {
@@ -30,19 +42,5 @@ $(document).ready(function() {
     }
 
 });
-
-// stupid unnecessary shit
- function coolMaskers(h1, h2) {
-   $(window).on('scroll', function() {
-     var height = $(document).scrollTop();
-     var masker1height = Math.abs($(h1).offset().top - height);
-     if (masker1height < 530) {
-       coolText(h1, 25);
-       setTimeout(function() { coolText(h2, 25); }, 1000);
-       $(window).off('scroll');
-    }
- });
- }
-coolMaskers(".masker1", ".masker2");
 
 });
