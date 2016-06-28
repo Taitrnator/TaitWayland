7f05ebabe6333bfee36098a6036f990e1161ef5f
Tait <Taitwayland@gmail.com>
2016-06-27 20:49:01 -0500

    fixing glitch image, totally not worth it

--- scripts/glitch.js
+++ scripts/glitch.js
@@ -4,8 +4,12 @@ var context = canvas.getContext("2d"),
 
 img.src = './images/TaitWayland.svg';
 img.onload = function() {
-  init();
+  setTimeout(function() { init(); }, 500);
 	window.onresize = init;
+	setTimeout(function() { closeContext(); }, 4000);
+	setTimeout(function() { $(".portrait").addClass("fadeInSoft");
+		$("canvas").hide();
+	}, 5000);
 };
 
 var init = function() {
@@ -26,6 +30,11 @@ var clear = function() {
 	context.fill();
 };
 
+var closeContext = function() {
+	clearInterval(glitchInterval);
+	context.clearRect(0, 0, w, h);
+};
+
 var glitchImg = function() {
 	for (var i = 0; i < randInt(1, 13); i++) {
 		var x = Math.random() * w;
@@ -40,3 +49,51 @@ var glitchImg = function() {
 var randInt = function(a, b) {
 	return ~~(Math.random() * (b - a) + a);
 };
+
+// makes the parallax elements
+function parallaxIt() {
+
+  // create variables
+  var fwindow = $(window);
+  var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+  // on window scroll event
+  fwindow.on('scroll resize', function() {
+    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  });
+
+  // for each of content parallax element
+  $('.portrait').each(function (index, e) {
+    var contentObj = $(this);
+    var fgOffset = parseInt(contentObj.offset().top);
+    var yPos;
+    var speed = (contentObj.data('speed') || 1 );
+
+    	fwindow.on('scroll resize', function (){
+      yPos = fgOffset - scrollTop / speed;
+
+      $contentObj.css('top', yPos);
+    });
+  });
+
+  // for each of background parallax element
+  $('.main-header').each(function(){
+    var backgroundObj = $(this);
+    var bgOffset = parseInt(backgroundObj.offset().top);
+    var yPos;
+    var coords;
+    var speed = ($backgroundObj.data('speed') || 0 );
+
+    	fwindow.on('scroll resize', function() {
+      yPos = - ((scrollTop - bgOffset) / speed);
+      coords = '50% '+ yPos + 'px';
+
+      backgroundObj.css({ backgroundPosition: coords });
+    });
+  });
+
+  // triggers winodw scroll for refresh
+  $fwindow.trigger('scroll');
+};
+
+parallaxIt();
