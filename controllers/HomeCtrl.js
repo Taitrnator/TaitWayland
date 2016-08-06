0b0912827748f4f479c21be20e18221f5e5bdd10
Tait <Taitwayland@gmail.com>
2016-08-06 11:24:31 -0500

    refactored portfolio scss

--- controllers/HomeCtrl.js
+++ controllers/HomeCtrl.js
@@ -10,18 +10,6 @@ TaitWayland.controller("HomeCtrl", function($scope, $http, $document) {
              $(e).shuffleLetters(fps);
            }
       }
-      // stupid unnecessary shit
-       function coolMaskers(h1, h2) {
-         $(window).on('scroll', function() {
-           var height = $(document).scrollTop();
-           var masker1height = Math.abs($(h1).offset().top - height);
-           if (masker1height < 530) {
-             coolText(h1, 25);
-             setTimeout(function() { coolText(h2, 25); }, 1000);
-             $(window).off('scroll');
-          }
-       });
-       }
 
        $scope.toSection = function(e) {
          $scope.menuToggle();
@@ -38,8 +26,6 @@ TaitWayland.controller("HomeCtrl", function($scope, $http, $document) {
       setTimeout(function() { coolText(".header-1", 44); }, 0);
       setTimeout(function() { coolText(".header-2", 44); }, 800);
       setTimeout(function() { coolText(".header-3"); }, 2500);
-
-      coolMaskers(".masker1", ".masker2");
 
     });
   });
